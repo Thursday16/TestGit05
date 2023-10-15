59351ac96daa1dd2e8a6e81bed708f9aceabb5f7
Thursday16 <104007103+Thursday16@users.noreply.github.com>
2023-10-15 22:42:37 +0700

    Update marbles.py

--- pi/walletapp/marbles.py
+++ pi/walletapp/marbles.py
@@ -25,3 +25,25 @@ def run():
     num_samples = 1000000  # You can adjust this number to increase or decrease accuracy
     pi_estimate = estimate_pi(num_samples)
     print(f"Estimated Pi: {pi_estimate}")
+---------------------------------
+import random
+
+def pi(num_samples):
+    count_circle = 0
+    count_square = 0
+
+    for i in range(num_samples):
+        x = random.randint(-100, 100)
+        y = random.randint(-100, 100)
+
+        if(x >= 0 and y >= 0):
+            count_square += 1
+        if(x ^ 2 + y ^ 2) <= 10000:
+            count_circle += 1
+
+    return count_circle / count_square
+
+def run():
+    num_samples = 1000000  # You can adjust this number to increase or decrease accuracy
+    pi_estimate = pi(num_samples)
+    print(f"Estimated Pi: {pi_estimate}")
